bcb8855845512bb05e42bc1f2191e687470a5834
Ricardo Melida <ricardomelida92@gmail.com>
2021-06-08 19:59:11 -0400

    se agrega metodo para obtener productos

--- index.js
+++ index.js
@@ -10,12 +10,26 @@ const Product = require('./models/product')
 app.use(bodyParser.urlencoded({ extended : false }));
 app.use(bodyParser.json());
 
-app.get('/api/product', (req, res) => {	
-	res.send(200, {products: []});
+app.get('/api/product', (req, res) => {
+	Product.find({}, (err, products) => {
+		if(err) return res.status(500).send({message: 'Error al realizar la peticion'});
+		
+		if(!products) return res.status(404).send({message: `El producto no existe`});
+	
+		res.send(200, { products });
+	});
 });
 
 app.get('/api/product/:productId', (req, res) => {
-	
+	let productId = req.params.productId;
+
+	Product.findById(productId, (err, product)=> {
+		if(err) return res.status(500).send({message: 'Error al realizar la peticion'});
+
+		if(!product) return res.status(404).send({message: `El producto no existe`});
+
+		res.status(200).send({ product });
+	})
 });
 
 app.post('/api/product', (req, res) => {
