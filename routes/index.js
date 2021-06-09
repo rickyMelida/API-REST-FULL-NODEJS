15e97c3054ff627385db636ad613f6e6444f15f6
Ricardo Melida <ricardomelida92@gmail.com>
2021-06-09 18:33:13 -0400

    Se ha aagregado el middlewares de autenticacion

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const productController = require('../controllers/product');
+const auth = require('../middlewares/auth');
 const api = express.Router();
+
 
 api.get('/product', productController.getProducts);
 api.get('/product/:productId', productController.getProduct);
 api.post('/product', productController.saveProduct)	
 api.put('/product/:productId', productController.updateProduct);
 api.delete('/product/:productId', productController.deleteProduct);
+
+//Prueba de autenticacion con token
+api.get('/private', auth, (req,res) =>{
+    res.status(200).send({message: 'Tienes acceso'});
+})
 
 module.exports = api;
