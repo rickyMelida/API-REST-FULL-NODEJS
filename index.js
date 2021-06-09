7a548dfebe8cbb3534a6f158da7dfc54c616538e
RickyMelidaTrabajo <ricardomelida92.trabajo@gmail.com>
2021-06-09 06:32:22 -0400

    Se agregan metodos para eliminar y actualizar

--- index.js
+++ index.js
@@ -3,19 +3,19 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const app = express();
-const PORT = process.env.PORT || 3001; 
+const PORT = process.env.PORT || 3001;
 
 const Product = require('./models/product')
 
-app.use(bodyParser.urlencoded({ extended : false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get('/api/product', (req, res) => {
 	Product.find({}, (err, products) => {
-		if(err) return res.status(500).send({message: 'Error al realizar la peticion'});
-		
-		if(!products) return res.status(404).send({message: `El producto no existe`});
-	
+		if (err) return res.status(500).send({ message: 'Error al realizar la peticion' });
+
+		if (!products) return res.status(404).send({ message: `El producto no existe` });
+
 		res.send(200, { products });
 	});
 });
@@ -23,10 +23,10 @@ app.get('/api/product', (req, res) => {
 app.get('/api/product/:productId', (req, res) => {
 	let productId = req.params.productId;
 
-	Product.findById(productId, (err, product)=> {
-		if(err) return res.status(500).send({message: 'Error al realizar la peticion'});
+	Product.findById(productId, (err, product) => {
+		if (err) return res.status(500).send({ message: 'Error al realizar la peticion' });
 
-		if(!product) return res.status(404).send({message: `El producto no existe`});
+		if (!product) return res.status(404).send({ message: `El producto no existe` });
 
 		res.status(200).send({ product });
 	})
@@ -43,26 +43,42 @@ app.post('/api/product', (req, res) => {
 	product.description = req.body.description;
 
 	product.save((err, productStored) => {
-		if(err) res.status(500).send({messsage: `Error al guardar en la BD:  ${err}` });
+		if (err) res.status(500).send({ messsage: `Error al guardar en la BD:  ${err}` });
 
 		res.status(200).send({ product: productStored });
 	})
 })
 
-app.put('/api/product/:productId', (req, res)=> {
+app.put('/api/product/:productId', (req, res) => {
+	let productId = req.params.productId;
+	let update = req.body;
+	Product.findByIdAndUpdate(productId, update, (err, productUpdated) => {
+		if (err) return res.status(500).send({ message: `Error al actualizar el producto` });
 
+		res.status(200).send({ product: productUpdated });
+
+	})
 });
 
 app.delete('/api/product/:productId', (req, res) => {
+	let productId = req.params.productId;
 
+	Product.findById(productId, (err, product) => {
+		if (err) return res.status(500).send({ message: `Error al borrar el producto` });
+
+		product.remove(err => {
+			if (err) return res.status(500).send({ message: `Error al borrar producto` });
+			res.status(200).send({ message: 'El producto ha sido eliminado' });
+		})
+	})
 });
 
 mongoose.connect('mongodb://localhost:27017/shop', (err, res) => {
-	if(err) {
+	if (err) {
 		return console.log('Error al conectar a la base de datos');
 	}
 	console.log('Conexion a la BD exitosa');
-	app.listen(PORT, ()=>{
+	app.listen(PORT, () => {
 		console.log(`Servidor corriendo en puerto ${PORT}`);
 	});
 });
